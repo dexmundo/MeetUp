b0f27352ba0707e79593bd0ef6fa430a9c0d185d
Sad7920 <87930878+Sad7920@users.noreply.github.com>
2025-01-28 13:27:31 +0530

    added my signature footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import Button from '@/components/Button';
 import { Toaster } from '@/components/ui/toaster';
 import { useToast } from '@/hooks/use-toast';
+import Link from 'next/link';
 
 export default function Home() {
   const router = useRouter();
@@ -24,21 +25,26 @@ export default function Home() {
   };
 
   return (
-    <div className='w-full h-screen flex flex-col items-center justify-center p-4 text-center bg-neutral-900 text-white'>
+    <div className='w-full h-screen  text-center bg-neutral-900 text-white'>
       <Toaster />
-      <h1 className='text-4xl font-bold mb-4'><span className='meetup-text text-5xl'>MeetUp</span> - Connect Instantly</h1>
-      <p className='text-lg text-neutral-400 mb-6'>No sign-ups, no downloads—just smooth, secure video calls with MeetUp. Stay connected with ease.</p>
-      <div className='flex flex-col gap-4 max-w-xs w-full'>
-        <input
-          placeholder='Enter Room ID'
-          value={roomId}
-          onChange={(e) => setRoomId(e.target.value)}
-          className='bg-neutral-800 text-white placeholder-neutral-500 px-4 py-2 w-full border border-neutral-700 rounded-md'
-        />
-        <Button text='Join a Room' onClick={joinRoom} />
+      <div className='flex flex-col p-4 items-center justify-center w-full h-[95vh] '>
+        <h1 className='text-4xl font-bold mb-4'><span className='meetup-text text-5xl'>MeetUp</span> - Connect Instantly</h1>
+        <p className='text-lg text-neutral-400 mb-6'>No sign-ups, no downloads—just smooth, secure video calls with MeetUp. Stay connected with ease.</p>
+        <div className='flex flex-col gap-4 max-w-xs w-full'>
+          <input
+            placeholder='Enter Room ID'
+            value={roomId}
+            onChange={(e) => setRoomId(e.target.value)}
+            className='bg-neutral-800 text-white placeholder-neutral-500 px-4 py-2 w-full border border-neutral-700 rounded-md'
+          />
+          <Button text='Join a Room' onClick={joinRoom} />
+        </div>
+        <span className='my-4 text-neutral-500'>------ OR ------</span>
+        <Button text='Create a New Room' onClick={createAndJoinRoom} />
       </div>
-      <span className='my-4 text-neutral-500'>------ OR ------</span>
-      <Button text='Create a New Room' onClick={createAndJoinRoom} />
+      <div className="w-full h-[5vh] bg-neutral-800 flex gap-1 items-center justify-center text-neutral-500">
+        Made with ❤️ by <Link href="https://sad.codes" className="  hover:opacity-75 text-gradient bg-gradient-to-br from-indigo-300 to-indigo-700 ">sad.codes</Link>
+      </div>
     </div>
   );
 }
